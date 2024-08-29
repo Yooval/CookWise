c4a2bd49e241f5506b9e0be850d3b92dd505f0dd
Yooval <116361337+Yooval@users.noreply.github.com>
2024-08-29 20:45:28 +0300

    Commit!

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,7 +2,6 @@
 Sample tests
 """
 from django.test import SimpleTestCase
-
 from app import calc
 
 
@@ -14,7 +13,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,11 @@
 """
 Django command to wait for the database to be available.
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -8,4 +13,15 @@ class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:  # chek if db is up
+            try:
+                self.check(databases=['default'])  # if db not ready-exeption
+                db_up = True  # if we got here so db is up
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)  # slepp for a second let db wake up
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
